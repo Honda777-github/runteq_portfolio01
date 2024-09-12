2c3eaaddd89b4d0e3b9f3e89a421d9014a04e2cd
satoshi_honda <hndsts@gmail.com>
2024-09-12 23:06:36 +0900

    コメント投稿機能ベータ版01

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,12 @@
 # end
 shops = [
   { name: 'ガラム・マサラ', address: '北海道札幌市中央区南1条西27丁目1-1', description: 'ダミーデータ', rating: 5, hot_rank: 1, img: "", category: "currymix" },
+  { name: 'コリアンダ', address: '北海道札幌市中央区西1条西2丁目1-5', description: 'ダミーデータ', rating: 3, hot_rank: 1, img: "", category: "currymix" },
   { name: '札幌スープカレー', address: '北海道札幌市東区北31条東15-1-1', description: 'ダミーデータ', rating: 5, hot_rank: 2, img: "", category: "soup"  },
-  { name: 'イエローカレー', address: '北海道札幌市東区東苗穂2条3-1-1', description: 'ダミーデータ', rating: 5, hot_rank: 5, img: "", category: "soup" }
+  { name: 'スープカレーhot', address: '北海道札幌市東区北3条5丁目2-3', description: 'ダミーデータ', rating: 5, hot_rank: 2, img: "", category: "soup"  },
+  { name: 'チキンカリーbard', address: '北海道札幌市南区6条2-1-1', description: 'ダミーデータ', rating: 5, hot_rank: 2, img: "", category: "currymix"  },
+  { name: 'イエローカレー', address: '北海道札幌市東区東苗穂2条3-1-1', description: 'ダミーデータ', rating: 5, hot_rank: 5, img: "", category: "soup" },
+  { name: 'グリーンーカレー', address: '北海道札幌市東区2条3-1', description: 'ダミーデータ', rating: 5, hot_rank: 5, img: "", category: "soup" },
+  { name: 'マンモスカレー', address: '北海道札幌市中央区東3条3-1-2', description: 'ダミーデータ', rating: 5, hot_rank: 5, img: "", category: "currymix" }
 ]
 Shop.create(shops)
